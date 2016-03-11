0c62f587aebaf063b22f1c336b334efd300aa945
Adrian Smith <agnomen@gmail.com>
2016-03-11 13:55:03 -0500

    Proof of concept.

--- src/tapreader.js
+++ src/tapreader.js
@@ -1,10 +1,24 @@
-var TAP_READER = (function () {
+(function () {
     "use strict";
     
-    window.onload = function(e) {
-        console.log("window.onload", e, Date.now());
+    var words = 4,
+        word = 0;
+    
+    function advanceWord() {
+        var id = "w" + word,
+            element = document.getElementById(id);
+        element.className = "";
+        word = (word + 1) % words;
+        id = "w" + word;
+        element = document.getElementById(id);
+        element.className = "active";
     }
     
-    return {
+    window.onload = function (e) {
+        console.log("window.onload", e, Date.now());
+        var goButton = document.getElementById("go");
+        
+        goButton.addEventListener("mousedown", advanceWord, true);
+        goButton.addEventListener("touchstart", advanceWord, true);
     };
-});
+}());
