cc88aeb0d21e2f75241b8c7ccda70043276821ba
Adrian Smith <agnomen@gmail.com>
2016-03-10 23:00:30 -0500

    Set up tap reader module.

---
+++ src/tapreader.js
@@ -0,0 +1,10 @@
+var TAP_READER = (function () {
+    "use strict";
+    
+    window.onload = function(e) {
+        console.log("window.onload", e, Date.now());
+    }
+    
+    return {
+    };
+});
